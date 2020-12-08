Fixed low-order scheme. Uncorrected solution gradient calculation for normalization.

pyfr/shapes.py
@@ -113,7 +113,6 @@ class BaseShape(object):
                   for kind, proj, norm in self.faces]
 
             m = m @ block_diag(fp)
-
         return m
 
     @lazyprop
@@ -166,12 +165,12 @@ class BaseShape(object):
         M = np.zeros((self.nupts, self.nfpts))
 
         solpts = self.upts[:p+1,0] # Hack
+
         rdpts = np.zeros(p+2)
         rdpts[0] = -1.
         rdpts[-1] = 1.
-        for i in range(p):
-            rdpts[i+1] = 0.5*(solpts[i] + solpts[i+1])
-        
+        rdpts[1:-1] = get_quadrule('line', 'gauss-legendre', p).pts
+
         vals = np.zeros(p+2)
         vals[0] = 1.
         g_left = interpolate.lagrange(rdpts, vals).deriv()
@@ -228,20 +227,19 @@ class BaseShape(object):
         return M
 
     @lazyprop
-
     # M3 matrix for shock points correction in subcell scheme
     def m13(self):
         p = self.order
         M = np.zeros((self.nupts, self.nfpts))
 
         solpts = self.upts[:p+1,0] # Hack
-        rdpts = np.zeros(p+2)
-        rdpts[0] = -1.
-        rdpts[-1] = 1.
-        for i in range(p):
-            rdpts[i+1] = 0.5*(solpts[i] + solpts[i+1])
 
-        dx = rdpts[1] - rdpts[0]
+        scpts = np.zeros(p+2)
+        scpts[0] = -1.
+        scpts[-1] = 1.
+        scpts[1:-1] = 0.5*(solpts[1:] + solpts[:-1])
+
+        dx = (scpts[1] - scpts[0])
 
         if self.ndims == 2:
             # HEX FACE ORDERING:
@@ -257,7 +255,7 @@ class BaseShape(object):
                 solidx = uidx(0, yidx)
                 M[solidx, fxm_idx] = 1./dx
                 solidx = uidx(p, yidx)
-                M[solidx, fxp_idx] =  1./dx
+                M[solidx, fxp_idx] = 1./dx
             for xidx in range(p+1):
                 fym_idx = faceidx(0,xidx)
                 fyp_idx = faceidx(2,xidx)
@@ -266,34 +264,11 @@ class BaseShape(object):
                 solidx = uidx(xidx, p)
                 M[solidx, fyp_idx] =  1./dx
         if self.ndims == 3:
-            # HEX FACE ORDERING:
-            # 0 -> z = -1
-            # 1 -> y = -1
-            # 2 -> x =  1
-            # 3 -> y =  1
-            # 4 -> x = -1
-            # 5 -> z =  1
-            raise NotImplementedError()
-            # faceidx = lambda face, a_idx, b_idx: face*(p+1)**2 + a_idx + b_idx*(p+1)
-            # uidx = lambda xidx, yidx, zidx: xidx + yidx*(p+1) + zidx*((p+1)**2)
-            # for yidx in range(p+1):
-            # 	for zidx in range(p+1):
-	           #      fxm_idx = faceidx(3,yidx)
-	           #      fxp_idx = faceidx(1,yidx)
-	           #      solidx = uidx(0, yidx)
-	           #      M[solidx, fxm_idx] = 1./dx
-	           #      solidx = uidx(p, yidx)
-	           #      M[solidx, fxp_idx] = 1./dx
-            # for xidx in range(p+1):
-            # 	for zidx in range(p+1):
-	           #      solidx = uidx(xidx, 0)
-	           #      M[solidx, fym_idx] = 1./dx
-	           #      solidx = uidx(xidx, p)
-	           #      M[solidx, fyp_idx] = 1./dx        
+            raise NotImplementedError()      
+
         return M
 
     @lazyprop
-
     # M0 matrix for interpolating subcells to flux points (constant)
     def m14(self):
         p = self.order
@@ -336,6 +311,76 @@ class BaseShape(object):
             # 4 -> x = -1
             # 5 -> z =  1
             raise NotImplementedError()
+
+        return M
+
+    @lazyprop
+    # M1 matrix for centered subcell derivative
+    def m15(self):
+        p = self.order
+        solpts = self.upts[:p+1,0] # Hack
+        
+        scpts = np.zeros(p+2)
+        scpts[0] = -1.
+        scpts[-1] = 1.
+        scpts[1:-1] = 0.5*(solpts[1:] + solpts[:-1])
+        M = np.zeros((self.nupts, 2*self.nupts))
+
+        if self.ndims == 2:
+            uidx = lambda xidx, yidx: xidx + yidx*(p+1)
+            for i in range(p+1):
+                for j in range(p+1):
+                    idx = uidx(i,j)
+                    ridx = uidx(min(i+1,p),j)
+                    lidx = uidx(max(0,i-1),j)
+                    tidx = self.nupts + uidx(i,min(j+1,p))
+                    bidx = self.nupts + uidx(i,max(0,j-1))
+                    dx = scpts[i+1] - scpts[i]
+                    dy = scpts[j+1] - scpts[j]
+
+                    M[idx, ridx] = 0.5/dx
+                    M[idx, lidx] = -0.5/dx
+                    M[idx, tidx] = 0.5/dy
+                    M[idx, bidx] = -0.5/dy
+        return M
+
+    @lazyprop
+    # M2 matrix for interpolating subcells flux (f,g,h) values to flux points (constant) and dotting with normal
+    def m16(self):
+        p = self.order
+        M = np.zeros((self.nfpts, self.nupts*self.ndims))
+
+        solpts = self.upts[:p+1,0] # Hack
+
+        if self.ndims == 2:
+            # HEX FACE ORDERING:
+            # 0 -> y = -1
+            # 1 -> x =  1
+            # 2 -> y =  1
+            # 3 -> x = -1
+            faceidx = lambda face, idx: face*(p+1) + idx
+            uidx = lambda xidx, yidx: xidx + yidx*(p+1)
+            for idx in range(p+1):
+                fxm_idx = faceidx(3,idx)
+                fxp_idx = faceidx(1,idx)
+                fym_idx = faceidx(0,idx)
+                fyp_idx = faceidx(2,idx)
+
+                solidx = uidx(0, idx)
+                M[fxm_idx, solidx] = -1.
+
+                solidx = uidx(p, idx)
+                M[fxp_idx, solidx] = 1.
+
+                solidx = uidx(idx, 0)
+                M[fym_idx, solidx+self.nupts] = -1.
+
+                solidx = uidx(idx, p)
+                M[fyp_idx, solidx+self.nupts] = 1.
+
+        if self.ndims == 3:
+            raise NotImplementedError()
+
         return M
 
     @lazyprop

pyfr/solvers/euler/elements.py
@@ -86,73 +86,30 @@ class EulerElements(BaseFluidElements, BaseAdvectionElements):
 
         if shock_capturing == 'riemann-difference':
             # Register the kernels
-            self._be.pointwise.register('pyfr.solvers.euler.kernels.rdshocksensor')
             self._be.pointwise.register('pyfr.solvers.euler.kernels.riemanndifference')
+            # self._be.pointwise.register('pyfr.solvers.euler.kernels.divf_LO')
+            # self._be.pointwise.register('pyfr.solvers.euler.kernels.divf_LO_centered')
             self._be.pointwise.register('pyfr.solvers.euler.kernels.residual')
+            self._be.pointwise.register('pyfr.solvers.euler.kernels.normalizeresidual')
+            self._be.pointwise.register('pyfr.solvers.euler.kernels.limitinterp')
 
             self.kernels['copy_soln_at_fpts'] = lambda: self._be.kernel(
                     'copy', self._scal_fpts_cpy, self._scal_fpts
                 )
 
-
-            # Shockcell is an elementwise value (0,1) dictating if there is a shock in the cell (1)
-            self.shockcell = self._be.matrix((1, self.neles), tags={'align'}, initval=np.zeros((1, self.neles)))
-
             self.residual = self._be.matrix((self.nupts, self.neles), tags={'align'}, initval=np.zeros((self.nupts, self.neles)))
 
-            # Sense shocks using density 
-            shockvar = 0
-            shocksensor = self.cfg.get('solver-riemann-difference', 'shock-sensor', 'none')
-
-            # Get matrices for convergence sensor
-            pstages = self.cfg.getliteral('solver-riemann-difference', 'order-stages', [1,2,3])
-            convvars = self.cfg.getliteral('solver-riemann-difference', 'shock-vars', [0,3])
-            if 'conv' in shocksensor and max(pstages) > self.basis.order:
-                raise ValueError('Invalid projection order for convergence stages. {0}'.format(pstages))
-
-            [diffxi, diffeta, diffzeta, proj1, proj2, proj3, quadwts] = self.generateProjectionMats(pstages)
-
-
-            # Sense shocks and choose FR or RD
-            self.kernels['rdshocksensor'] = lambda: self._be.kernel(
-               'rdshocksensor', tplargs=tplargs, dims=[self.neles], u=self.scal_upts_inb, 
-               shockcell=self.shockcell, divf_fr=self.scal_upts_outb, divf_rd=self._scal_upts_cpy,
-               usmats=self.ele_smat_at('upts'), rcpdjac=self.rcpdjac_at('upts'), ploc=self.ploc_at('upts')
-            )
-
-            diffmat = self.generateRDDiffMat()
+            rdpts = self.getRDpts()
+            diffmatRD = self.generateRDDiffMat(rdpts)
             rsolver = self.cfg.get('solver-interfaces', 'riemann-solver')
 
             # Template arguments
             tplargs = dict(
                 nvars=self.nvars, nupts=self.nupts, nfpts=self.nfpts, ndims=self.ndims,
-                c=self.cfg.items_as('constants', float), shocksensor=shocksensor,
-                crd=self.cfg.items_as('solver-riemann-difference', float), convvars=convvars,
-                order=self.basis.order, svar=shockvar, diffmat=diffmat, rsolver=rsolver,
-                vdm=self.basis.ubasis.vdm.T, 
-                invvdm=self.basis.ubasis.invvdm.T, 
-                ubdegs=[sum(dd) for dd in self.basis.ubasis.degrees],
-                l1degs=[max(dd) for dd in self.basis.ubasis.degrees],
-                diffxi=diffxi, diffeta=diffeta, diffzeta=diffzeta,
-                proj1=proj1, proj2=proj2, proj3=proj3, quadwts=quadwts,
+                c=self.cfg.items_as('constants', float), 
+                crd=self.cfg.items_as('solver-riemann-difference', float), 
+                order=self.basis.order, diffmatRD=diffmatRD, rsolver=rsolver,
                 dt=self.cfg.get('solver-time-integrator', 'dt')
-            )
-
-            # Smats order is 
-            # [dxi/dx, deta/dx, dxi/dy, deta/dy]
-            # [dxi/dx, deta/dx, dzeta/dx, dxi/dy, deta/dy, dzeta/dy,  dxi/dz, deta/dz, dzeta/dz]
-            self.kernels['riemanndifference'] = lambda: self._be.kernel(
-                'riemanndifference', tplargs=tplargs, dims=[self.neles],
-                u=self.scal_upts_inb, plocu=self.ploc_at('upts'), usmats=self.ele_smat_at('upts'),
-                uf=self._scal_fpts_cpy, fsmats=self.ele_smat_at('fpts'), divf=self._scal_upts_cpy
-            )
-
-            diffmat = self.generateSubcellDiffMat()
-            tplargs["diffmat"] = diffmat
-            self.kernels['divf_LO'] = lambda: self._be.kernel(
-                'riemanndifference', tplargs=tplargs, dims=[self.neles],
-                u=self.scal_upts_inb, plocu=self.ploc_at('upts'), usmats=self.ele_smat_at('upts'),
-                uf=self._scal_fpts_cpy, fsmats=self.ele_smat_at('fpts'), divf=self.scal_upts_outb
             )
 
             self.kernels['residual'] = lambda: self._be.kernel(
@@ -160,17 +117,66 @@ class EulerElements(BaseFluidElements, BaseAdvectionElements):
                 rcpdjac=self.rcpdjac_at('upts'), u=self.scal_upts_inb, r=self.residual
             )
 
-    def generateRDDiffMat(self):
+            diffmatSOL = self.generateSolDiffMat(rdpts)
+            tplargs["diffmatSOL"] = diffmatSOL
+            tplargs["kvars"] = 3
+            tplargs["tol"] = 1e-8
+
+            self.kernels['normalizeresidual'] = lambda: self._be.kernel(
+                'normalizeresidual', tplargs=tplargs, dims=[self.neles],
+                u=self.scal_upts_inb, r=self.residual, usmats=self.ele_smat_at('upts'),
+                rcpdjac=self.rcpdjac_at('upts')
+            )
+
+            self.kernels['limitinterp'] = lambda: self._be.kernel(
+                'limitinterp', tplargs=tplargs, dims=[self.nfpts, self.neles],
+                u=self._scal_fpts
+            ) 
+
+            interpmat = self.generateInterpMat(rdpts)
+            tplargs["interpmat"] = interpmat
+            tplargs["e_max"] = self.cfg.get('solver-riemann-difference', 'e_max')
+            # Smats order is 
+            # [dxi/dx, deta/dx, dxi/dy, deta/dy]
+            # [dxi/dx, deta/dx, dzeta/dx, dxi/dy, deta/dy, dzeta/dy,  dxi/dz, deta/dz, dzeta/dz]
+            self.kernels['riemanndifference'] = lambda: self._be.kernel(
+                'riemanndifference', tplargs=tplargs, dims=[self.neles],
+                u=self.scal_upts_inb, plocu=self.ploc_at('upts'), usmats=self.ele_smat_at('upts'),
+                uf=self._scal_fpts_cpy, fsmats=self.ele_smat_at('fpts'), divf=self.scal_upts_outb,
+                res=self.residual
+            )
+
+    def getRDpts(self):
+        p = self.basis.order
+        solpts = self.basis.upts[:p+1,0] # Hack
+
+        rdpts = np.zeros(p+2)
+        rdpts[0] = -1.
+        rdpts[-1] = 1.
+        rdpts[1:-1] = get_quadrule('line', 'gauss-legendre', p).pts
+
+        return rdpts
+
+    def generateInterpMat(self, rdpts):
+        p = self.basis.order
+        M = np.zeros((p+2, p+1))
+
+        solpts = self.basis.upts[:p+1,0] # Hack
+       
+        for i in range(p+1):
+            vals = np.zeros(p+1)
+            vals[i] = 1.
+            lag = interpolate.lagrange(solpts, vals)
+            for j in range(p+2):
+                M[j,i] = lag(rdpts[j])
+        return M   
+
+    def generateRDDiffMat(self, rdpts):
         p = self.basis.order
         M = np.zeros((p+1, p+2))
 
         solpts = self.basis.upts[:p+1,0] # Hack
-        rdpts = np.zeros(p+2)
-        rdpts[0] = -1.
-        rdpts[-1] = 1.
-        for i in range(p):
-            rdpts[i+1] = 0.5*(solpts[i] + solpts[i+1])
-        
+       
         for i in range(p+2):
             vals = np.zeros(p+2)
             vals[i] = 1.
@@ -179,115 +185,30 @@ class EulerElements(BaseFluidElements, BaseAdvectionElements):
                 M[j,i] = dlag(solpts[j])
         return M
 
-    def generateSubcellDiffMat(self):
+    def generateSubcellDiffMat(self, rdpts):
         p = self.basis.order
         M = np.zeros((p+1, p+2))
 
-        solpts = self.basis.upts[:p+1,0] # Hack
-        rdpts = np.zeros(p+2)
-        rdpts[0] = -1.
-        rdpts[-1] = 1.
-        for i in range(p):
-            rdpts[i+1] = 0.5*(solpts[i] + solpts[i+1])
-
         for i in range(p+1):
-        	dx = rdpts[i+1] - rdpts[i]
-        	M[i,i] = -1./dx
-        	M[i,i+1] = 1./dx
+            dx = rdpts[i+1] - rdpts[i]
+            M[i,i] = -1./dx
+            M[i,i+1] = 1./dx
         return M
 
-
-    def generateProjectionMats(self, pstages):
-        # p1pts = np.array([-0.5773502, 0.5773502])
-        # p2pts = np.array([-0.7745966, 0.0, 0.7745966])
-        # p3pts = np.array([-0.8611363, -0.3399810, -0.3399810, -0.8611363])
-
-        upts =  self.basis.upts
-        solpts = self.basis.upts[:self.basis.order+1,0] # Hack
+    def generateSolDiffMat(self, rdpts):
         p = self.basis.order
-        if self.ndims == 2:
-            # # Swap ordering of points for legvander2d
-            # rsupts = np.reshape(upts, (self.basis.order+1, self.basis.order+1, 2))
-            # rsupts = rsupts.swapaxes(0,1)
-            # rsupts = np.reshape(rsupts, (-1, 2))
+        M = np.zeros((p+1, p+1))
 
-            # # Generate inverse VDM matrix for calculating Legendre modes
-            # vdm  = np.polynomial.legendre.legvander2d(rsupts[:,0], rsupts[:,1], [self.basis.order]*2)
-            # invvdm = np.linalg.inv(vdm)
-            vdm = self.basis.ubasis.vdm.T
-            invvdm = np.linalg.inv(vdm)
-            
-            # Generate VDM matrices
-            vdm1 = copy.copy(vdm)
-            vdm2 = copy.copy(vdm)
-            vdm3 = copy.copy(vdm)
+        for i in range(p+1):
+            dx = rdpts[i+1] - rdpts[i]
+            if i == 0:
+                M[i,i] = -1./dx
+                M[i,i+1] = 1./dx
+            elif i == p:
+                M[i,i-1] = -1./dx
+                M[i,i] = 1./dx
+            else:
+                M[i,i-1] = -0.5/dx
+                M[i,i+1] = 0.5/dx
+        return M
 
-            modesfac1 = np.zeros((p+1, p+1))
-            modesfac2 = np.zeros((p+1, p+1))
-            modesfac3 = np.zeros((p+1, p+1))
-
-            modesfac1[:pstages[0]+1, :pstages[0]+1] = 1.0
-            modesfac2[:pstages[1]+1, :pstages[1]+1] = 1.0
-            modesfac3[:pstages[2]+1, :pstages[2]+1] = 1.0
-
-            modesfac1 = np.reshape(modesfac1, (-1))
-            modesfac2 = np.reshape(modesfac2, (-1))
-            modesfac3 = np.reshape(modesfac3, (-1))
-
-            # Zero higher order modes
-            for j in range(self.nupts):
-                vdm1[:, j] *= modesfac1[j]
-                vdm2[:, j] *= modesfac2[j]
-                vdm3[:, j] *= modesfac3[j]
-
-            # vdm1[:,(pstages[0]+1)**self.ndims:] *= 0
-            # vdm2[:,(pstages[1]+1)**self.ndims:] *= 0
-            # vdm3[:,(pstages[2]+1)**self.ndims:] *= 0
-
-            # Create projection matrices
-            proj1 = np.matmul(vdm1, invvdm)
-            proj2 = np.matmul(vdm2, invvdm)
-            proj3 = np.matmul(vdm3, invvdm)
-
-            proj1[np.abs(proj1) < 1e-7] = 0.0
-            proj2[np.abs(proj2) < 1e-7] = 0.0
-            proj3[np.abs(proj3) < 1e-7] = 0.0
-
-            # Create differentiation matrices
-            nptsx = self.basis.order+1
-            nptsy = self.basis.order+1
-            npts = nptsx*nptsy
-
-            Mxi = np.zeros((npts, npts))
-            Meta = np.zeros((npts, npts))
-
-            for i in range(nptsx):
-                for j in range(nptsy):
-                    vals = np.zeros(nptsx)
-                    vals[i] = 1.
-                    lag_xi = interpolate.lagrange(solpts, vals)
-                    dlag_xi = lag_xi.deriv()
-                    vals = np.zeros(nptsy)
-                    vals[j] = 1.
-                    lag_eta = interpolate.lagrange(solpts, vals)
-                    dlag_eta = lag_eta.deriv()
-                    for di in range(nptsx):
-                        for dj in range(nptsy):
-                            x = solpts[di]
-                            y = solpts[dj]
-                            Mxi[di+dj*nptsx, i+j*nptsx] = dlag_xi(x)*lag_eta(y)
-                            Meta[di+dj*nptsx, i+j*nptsx] = lag_xi(x)*dlag_eta(y)
-
-            # Get quadrature weights
-            eletype = 'quad'
-            rule = self.cfg.get('solver-elements-{0}'.format(eletype), 'soln-pts')
-            wts = get_quadrule(eletype, rule=rule, npts=len(self.basis.upts), qdeg=self.basis.order, flags=None).wts
-
-            return [Mxi, Meta, None, proj1, proj2, proj3, wts]
-        elif self.ndims == 3:
-            raise NotImplementedError()
-            #invvdm = np.linalg.inv(np.polynomial.legendre.legvander3d(upts[:,0], upts[:,1], upts[:,2], [self.basis.order]*3))
-
-        invvdm[np.abs(invvdm) < 1e-7] = 0.0
-
-        return [vdm, invvdm] 