644ff69ab6db9427ba37f910ae8993418909849e
tarikdzanic <41964501+tarikdzanic@users.noreply.github.com>
2022-07-02 21:55:08 -0400

    Fixes

--- pyfr/shapes.py
+++ pyfr/shapes.py
@@ -161,8 +161,13 @@ class BaseShape:
 
     @cached_property
     def upts(self):
-        rname = self.cfg.get(f'solver-elements-{self.name}', 'soln-pts')
-        return get_quadrule(self.name, rname, self.nupts).pts
+        if self.name in ['tetl', 'tetr']:
+            rname = self.cfg.get(f'solver-elements-tet', 'soln-pts')
+            return get_quadrule('tet', rname, self.nupts).pts
+        else:
+            rname = self.cfg.get(f'solver-elements-{self.name}', 'soln-pts')
+            return get_quadrule(self.name, rname, self.nupts).pts
+        
 
     def _get_qrule(self, eleint, kind, **kwargs):
         sect = f'solver-{eleint}-{kind}'
@@ -506,48 +511,432 @@ class PriShape(BaseShape):
                 for p in pts1d[:(sptord + 1 - i)]]
 
 
+class TetLShape(BaseShape):
+
+	'''
+	This tet defined by points
+	
+	(-1, -1, -1)
+	( 1, -1, -1)
+	(-1,  1, -1)
+	( 0,  0,  1)
+
+	Faces:
+	bottom
+	south
+	west
+	north-east
+
+	''' 
+
+	name = 'tetl'
+	ndims = 3
+
+	# nspts = n*(n + 1)*(n + 2)/6
+	npts_coeffs = [1, 3, 2, 0]
+	npts_cdenom = 6
+
+	# Faces: type, reference-to-face projection, normal
+
+	# CHANGE THIS TO NEW TET
+
+
+	faces = [
+		('tri', lambda s, t: (s, t, -1), (0, 0, -1)),
+		('tri', lambda s, t: (s + (t + 1)/2, (t - 1)/2, t), (0, -1, 0.5)),
+		('tri', lambda s, t: ((t - 1)/2, s + (t + 1)/2, t), (-1, 0, 0.5)),
+		('tri', lambda s, t: (-s - (t + 1)/2, s + (t + 1)/2, t), (1, 1, 0))
+	]
+
+
+	def __init__(self, nspts, cfg):
+		super().__init__(nspts, cfg)
+		# Move upper sol point to (0,0,1) (instead of (-1, -1, 1))
+		rfac = 0.5*(self.upts[:,2] + 1.)
+		self.upts[:,0] += rfac
+		self.upts[:,1] += rfac
+		self.ubasis = get_polybasis('tetl', self.order + 1, self.upts)
+
+	@classmethod
+	def std_ele(cls, sptord):
+		pts1d = np.linspace(-1, 1, sptord + 1)
+
+		vals = [(p, q, r)
+				for i, r in enumerate(pts1d)
+				for j, q in enumerate(pts1d[:(sptord + 1 - i)])
+				for p in pts1d[:(sptord + 1 - i - j)]]
+
+		# Transform to new tet (top point moves from (-1,-1) tp (0,0))
+		newvals = []
+		for (p,q,r) in vals:
+			rfac = (r + 1.0)/2.0
+			pnew = p + rfac
+			qnew = q + rfac
+			newvals.append( (pnew, qnew, r))
+
+		return newvals
+
+class TetRShape(BaseShape):
+
+	'''
+	This tet defined by points (TetL rotated 180 deg)
+	
+	( 1,  1)
+	(-1,  1)
+	( 1, -1)
+	( 0,  0)
+
+	Faces:
+	bottom
+	north
+	east
+	south-west
+	''' 
+
+	name = 'tetr'
+	ndims = 3
+
+	# nspts = n*(n + 1)*(n + 2)/6
+	npts_coeffs = [1, 3, 2, 0]
+	npts_cdenom = 6
+
+	# Faces: type, reference-to-face projection, normal
+
+	faces = [
+
+		('tri', lambda s, t: (t, s, -1), (0, 0, -1)),
+		('tri', lambda s, t: ((t - 1)/2, s + (t + 1)/2, t), (-1, 0, 0.5)),
+		('tri', lambda s, t: (s + (t + 1)/2, (t - 1)/2, t), (0, -1, 0.5)),
+		('tri', lambda s, t: (s + (t + 1)/2, -s - (t + 1)/2, t), (1, 1, 0))
+	]
+
+	def __init__(self, nspts, cfg):
+		super().__init__(nspts, cfg)
+		# Move upper sol point to (0,0,1) (instead of (-1, -1, 1))
+		rfac = 0.5*(self.upts[:,2] + 1.)
+		self.upts[:,0] += rfac
+		self.upts[:,1] += rfac
+		# Flip upts/fpts
+		self.upts[:,:2] *= -1
+		self.fpts[:,:2] *= -1
+		self.ubasis = get_polybasis('tetr', self.order + 1, self.upts)
+
+	@classmethod
+	def std_ele(cls, sptord):
+		pts1d = np.linspace(-1, 1, sptord + 1)
+
+		vals = [(p, q, r)
+				for i, r in enumerate(pts1d)
+				for j, q in enumerate(pts1d[:(sptord + 1 - i)])
+				for p in pts1d[:(sptord + 1 - i - j)]]
+
+		# Transform to new tet (top point moves from (-1,-1) tp (0,0))
+		# and rotate 180 deg
+		newvals = []
+		for (p,q,r) in vals:
+			rfac = (r + 1.0)/2.0
+			pnew = p + rfac
+			qnew = q + rfac
+			newvals.append( (-pnew, -qnew, r)) # rotate
+
+		return newvals
+
 class PyrShape(BaseShape):
-    name = 'pyr'
-    ndims = 3
+	name = 'pyr'
+	ndims = 3
 
-    # nspts = n*(n + 1)*(2*n + 1)/6
-    npts_coeffs = [2, 3, 1, 0]
-    npts_cdenom = 6
 
-    # Faces: type, reference-to-face projection, normal
-    faces = [
-        ('quad', lambda s, t: (s, t, -1), (0, 0, -1)),
-        ('tri', lambda s, t: (s + (t + 1)/2, (t - 1)/2, t), (0, -1, 0.5)),
-        ('tri', lambda s, t: ((1 - t)/2, -s - (t + 1)/2, t), (1, 0, 0.5)),
-        ('tri', lambda s, t: (-s - (t + 1)/2, (1 - t)/2, t), (0, 1, 0.5)),
-        ('tri', lambda s, t: ((t - 1)/2, s + (t + 1)/2, t), (-1, 0, 0.5)),
-    ]
 
-    # Jacobian expressions for a linear element
-    jac_exprs = [
-        ['((1 - x[1])*V[1][0] + (-x[1] - 1)*V[2][0] +'
-         ' (x[1] - 1)*V[0][0] + (x[1] + 1)*V[3][0])/4',
-         '((1 - x[1])*V[1][1] + (-x[1] - 1)*V[2][1] +'
-         '(x[1] - 1)*V[0][1] + (x[1] + 1)*V[3][1])/4',
-         '((1 - x[1])*V[1][2] + (-x[1] - 1)*V[2][2] +'
-         ' (x[1] - 1)*V[0][2] + (x[1] + 1)*V[3][2])/4'],
-        ['((1 - x[0])*V[2][0] + (-x[0] - 1)*V[1][0] +'
-         ' (x[0] - 1)*V[0][0] + (x[0] + 1)*V[3][0])/4',
-         '((1 - x[0])*V[2][1] + (-x[0] - 1)*V[1][1] +'
-         ' (x[0] - 1)*V[0][1] + (x[0] + 1)*V[3][1])/4',
-         '((1 - x[0])*V[2][2] + (-x[0] - 1)*V[1][2] +'
-         '(x[0] - 1)*V[0][2] + (x[0] + 1)*V[3][2])/4'],
-        ['(-V[0][0] - V[1][0] - V[2][0] - V[3][0] + 4*V[4][0])/8',
-         '(-V[0][1] - V[1][1] - V[2][1] - V[3][1] + 4*V[4][1])/8',
-         '(-V[0][2] - V[1][2] - V[2][2] - V[3][2] + 4*V[4][2])/8']
-    ]
+	# nspts = n*(n + 1)*(2*n + 1)/6
+	npts_coeffs = [2, 3, 1, 0]
+	npts_cdenom = 6
 
-    @classmethod
-    def std_ele(cls, sptord):
-        npts1d = 2*sptord + 1
-        pts1d = np.linspace(-1, 1, npts1d)
+	# Faces: type, reference-to-face projection, normal
+	'''
+	Faces:
+	0: bottom
+	1: south
+	2: east
+	3: north
+	4: west
 
-        return [(p, q, r)
-                for i, r in enumerate(pts1d[::2])
-                for q in pts1d[i:npts1d - i:2]
-                for p in pts1d[i:npts1d - i:2]]
+	'''
+	faces = [
+		('quad', lambda s, t: (s, t, -1), (0, 0, -1)),
+		('tri', lambda s, t: (s + (t + 1)/2, (t - 1)/2, t), (0, -1, 0.5)),
+		('tri', lambda s, t: ((1 - t)/2, -s - (t + 1)/2, t), (1, 0, 0.5)),
+		('tri', lambda s, t: (-s - (t + 1)/2, (1 - t)/2, t), (0, 1, 0.5)),
+		('tri', lambda s, t: ((t - 1)/2, s + (t + 1)/2, t), (-1, 0, 0.5)),
+	]
+
+	# Jacobian expressions for a linear element
+	jac_exprs = [
+		['((1 - x[1])*V[1][0] + (-x[1] - 1)*V[2][0] +'
+		 ' (x[1] - 1)*V[0][0] + (x[1] + 1)*V[3][0])/4',
+		 '((1 - x[1])*V[1][1] + (-x[1] - 1)*V[2][1] +'
+		 '(x[1] - 1)*V[0][1] + (x[1] + 1)*V[3][1])/4',
+		 '((1 - x[1])*V[1][2] + (-x[1] - 1)*V[2][2] +'
+		 ' (x[1] - 1)*V[0][2] + (x[1] + 1)*V[3][2])/4'],
+		['((1 - x[0])*V[2][0] + (-x[0] - 1)*V[1][0] +'
+		 ' (x[0] - 1)*V[0][0] + (x[0] + 1)*V[3][0])/4',
+		 '((1 - x[0])*V[2][1] + (-x[0] - 1)*V[1][1] +'
+		 ' (x[0] - 1)*V[0][1] + (x[0] + 1)*V[3][1])/4',
+		 '((1 - x[0])*V[2][2] + (-x[0] - 1)*V[1][2] +'
+		 '(x[0] - 1)*V[0][2] + (x[0] + 1)*V[3][2])/4'],
+		['(-V[0][0] - V[1][0] - V[2][0] - V[3][0] + 4*V[4][0])/8',
+		 '(-V[0][1] - V[1][1] - V[2][1] - V[3][1] + 4*V[4][1])/8',
+		 '(-V[0][2] - V[1][2] - V[2][2] - V[3][2] + 4*V[4][2])/8']
+	]
+
+
+
+	def __init__(self, nspts, cfg):
+		super().__init__(nspts, cfg)
+		self.tetl = TetLShape(False, self.cfg)
+		self.tetr = TetRShape(False, self.cfg)
+
+		p = self.order
+		self.ntripts = (p+1)*(p+2)//2
+		self.nquadpts = (p+1)**2
+
+		self.make_fpts_map()
+		self.make_upts_map()
+
+
+	@classmethod
+	def std_ele(cls, sptord):
+		npts1d = 2*sptord + 1
+		pts1d = np.linspace(-1, 1, npts1d)
+
+		return [(p, q, r)
+				for i, r in enumerate(pts1d[::2])
+				for q in pts1d[i:npts1d - i:2]
+				for p in pts1d[i:npts1d - i:2]]
+
+	def make_fpts_map(self):
+		self.fpts_map = [None]*self.nfpts
+		self.fpts_idxs = [None]*self.nfpts
+		n = 0
+		# Bottom quad points
+		# Format: i/x changes first, then j/y
+
+		tetl_bot = self.tetl.fpts[:self.ntripts]
+		tetr_bot = self.tetr.fpts[:self.ntripts]
+
+		tol = 1e-8
+		for i in range(self.nquadpts):
+			coord = self.fpts[n, :]
+			[x,y,z] = coord
+			# iidx = i % (self.order+1)
+			# negiidx = (self.order) - iidx
+			# jidx = i // (self.order+1)
+
+			if np.abs(x+y) < tol:
+				tag = 'm'
+				idxl = np.where((np.abs(tetl_bot - coord) < tol).all(axis=1))[0][0]
+				idxr = np.where((np.abs(tetr_bot - coord) < tol).all(axis=1))[0][0]
+				assert idxl == idxr, f'{idxl}, {idxr}'
+				self.fpts_idxs[n] = idxl
+			elif x + y < -tol:
+				tag = 'l'
+				self.fpts_idxs[n] = np.where((np.abs(tetl_bot - coord) < tol).all(axis=1))[0][0]
+			else:
+				tag = 'r'
+				self.fpts_idxs[n] = np.where((np.abs(tetr_bot - coord) < tol).all(axis=1))[0][0]
+
+			self.fpts_map[n] = tag
+
+			n += 1
+
+
+		'''
+		
+		Pyr faces:
+			0: bottom
+			1: south
+			2: east
+			3: north
+			4: west
+
+		TetL faces:
+			0: bottom
+			1: south
+			2: west
+			3: north-east
+
+		TetR faces:
+			0: bottom
+			1: east
+			2: north
+			3: south-west
+		
+		'''
+		for face in range(1,5):
+			pyr_coords = self.fpts[self.nquadpts + (face-1)*self.ntripts: \
+								   self.nquadpts + (face)*self.ntripts, : ]
+			if face == 1:
+				tag = 'l'
+				offset = 1*self.ntripts
+				tet_coords = self.tetl.fpts[offset:offset+self.ntripts,:]
+			elif face == 2:
+				tag = 'r'
+				offset = 1*self.ntripts
+				tet_coords = self.tetr.fpts[offset:offset+self.ntripts,:]
+			elif face == 3:
+				tag = 'r'
+				offset = 2*self.ntripts
+				tet_coords = self.tetr.fpts[offset:offset+self.ntripts,:]
+			elif face == 4:
+				tag = 'l'
+				offset = 2*self.ntripts
+				tet_coords = self.tetl.fpts[offset:offset+self.ntripts,:]
+
+			for i in range(self.ntripts):
+				self.fpts_map[n] = tag
+				coord = self.fpts[n, :]
+
+				locidx = np.where((tet_coords == coord).all(axis=1))[0][0]
+				self.fpts_idxs[n] = offset + locidx
+				n += 1
+
+	def make_upts_map(self):
+		self.upts_map = [None]*self.nupts
+		self.upts_idxs = [None]*self.nupts
+
+		tol = 1e-8
+		for i in range(self.nupts):
+			coord = self.upts[i, :]
+			(x,y,z) = coord
+
+			if np.abs(x+y) < tol: # Middle/diag points
+				lidx = np.where((self.tetl.upts == coord).all(axis=1))[0][0]
+				ridx = np.where((self.tetr.upts == coord).all(axis=1))[0][0]
+				self.upts_map[i] = 'm'
+				self.upts_idxs[i] = (lidx, ridx)
+			elif x+y > tol:
+				self.upts_map[i] = 'r'
+				idx = np.where((self.tetr.upts == coord).all(axis=1))[0][0]
+				self.upts_idxs[i] = idx
+			elif x+y < -tol:
+				self.upts_map[i] = 'l'
+				idx = np.where((self.tetl.upts == coord).all(axis=1))[0][0]
+				self.upts_idxs[i] = idx
+
+	def expand_upts_row(self, row, side):
+		newrow = np.zeros(self.nupts)
+		#Arbitrarily choose right side to include midpoints as well
+		for i,v in enumerate(row):
+			newidx = None
+			if side == 'right':
+				for j, vv in enumerate(self.upts_idxs):
+					if isinstance(vv, np.int64):
+						if vv == i and self.upts_map[j] == 'r':
+							newidx = j
+					elif isinstance(vv, tuple):
+						if vv[1] == i:
+							newidx = j
+			elif side == 'left':
+				for j, vv in enumerate(self.upts_idxs):
+					if isinstance(vv, np.int64):
+						if vv == i and self.upts_map[j] == 'l':
+							newidx = j
+					elif isinstance(vv, tuple):
+						if vv[0] == i:
+							newidx = j
+
+			assert newidx is not None
+			newrow[newidx] = v
+		return newrow
+
+	def expand_fpts_row(self, row, side):
+		newrow = np.zeros(self.nfpts)
+		#Arbitrarily choose right side to include midpoints as well
+		for i,v in enumerate(row):
+			newidx = None
+			if side == 'right':
+				for j, vv in enumerate(self.fpts_idxs):
+					if vv == i and self.fpts_map[j] in ['r', 'm']:
+						newidx = j
+			elif side == 'left':
+				for j, vv in enumerate(self.fpts_idxs):
+					if vv == i and self.fpts_map[j] in ['l', 'm']:
+						newidx = j
+
+			assert newidx is not None
+			newrow[newidx] = v
+		return newrow
+	
+	@cached_property
+	def m0(self):
+		M = np.zeros((self.nfpts, self.nupts))
+		tol = 1e-8
+
+		for i, (x,y,z) in enumerate(self.fpts):
+			if np.abs(x + y) < tol: # Midpoints
+				rowl = self.tetl.ubasis.nodal_basis_at([[x,y,z]])[0]
+				rowr = self.tetr.ubasis.nodal_basis_at([[x,y,z]])[0]
+				M[i, :] =  0.5*self.expand_upts_row(rowl, 'left')
+				M[i, :] += 0.5*self.expand_upts_row(rowr, 'right')
+			elif (x + y) > tol: # Right
+				row = self.tetr.ubasis.nodal_basis_at([[x,y,z]])[0]
+				M[i, :] = self.expand_upts_row(row, 'right')
+			else: # Left
+				row = self.tetl.ubasis.nodal_basis_at([[x,y,z]])[0]
+				M[i, :] = self.expand_upts_row(row, 'left')
+
+		return M
+
+	@cached_property
+	def m1(self):
+		M = np.zeros((self.nupts, 3*self.nupts))
+		tol = 1e-8
+
+		def expand_grad(mat, side):			
+			r1 = self.expand_upts_row(mat[0,0,:], side)
+			r2 = self.expand_upts_row(mat[0,1,:], side)
+			r3 = self.expand_upts_row(mat[0,2,:], side)
+
+			newmat = np.array([r1, r2, r3])
+			return newmat
+
+		for i, (x,y,z) in enumerate(self.upts):
+			if np.abs(x + y) < tol: # Midpoints
+				ml = np.rollaxis(self.tetl.ubasis.jac_nodal_basis_at([[x,y,z]]), 2)
+				ml = expand_grad(ml, 'left')
+
+				mr = np.rollaxis(self.tetr.ubasis.jac_nodal_basis_at([[x,y,z]]), 2)
+				mr = expand_grad(mr, 'right')
+
+				M[i, :] =  0.5*ml.reshape(1, -1) + 0.5*mr.reshape(1, -1)
+			elif (x + y) > tol: # Right
+				m = np.rollaxis(self.tetr.ubasis.jac_nodal_basis_at([[x,y,z]]), 2)
+				m = expand_grad(m, 'right')
+				M[i, :] = m.reshape(1, -1)
+			else: # Left 
+				m = np.rollaxis(self.tetl.ubasis.jac_nodal_basis_at([[x,y,z]]), 2)
+				m = expand_grad(m, 'left')
+				M[i, :] = m.reshape(1, -1)
+
+		return M
+
+	@cached_property
+	def m3(self):
+		M = np.zeros((self.nupts, self.nfpts))
+		tol = 1e-8
+
+		# Remove contribution of interior interface (tet diagonal)
+		def remove_diag(m):
+			return m[:-self.ntripts]
+
+
+		for i, (x,y,z) in enumerate(self.upts):
+			ml = remove_diag(self.tetl.gbasis_at([[x,y,z]])[0])
+			mr = remove_diag(self.tetr.gbasis_at([[x,y,z]])[0])
+
+			if np.abs(x + y) < tol: # Midpoints
+				M[i, :] = 0.5*(self.expand_fpts_row(ml, 'left') + self.expand_fpts_row(mr, 'right'))
+			elif (x + y) > tol: # Right
+				M[i, :] = self.expand_fpts_row(mr, 'right')
+			else: # Left 
+				M[i, :] = self.expand_fpts_row(ml, 'left')
+		return M
